c557952d53d11d9a7b0bcc0fb9cb19614a86be51
BARRY MCMULLAN <barry.mcmullan@mac.com>
2013-07-20 01:18:32 -0600

    Added some comments.

--- iPadPager/OSWViewController.m
+++ iPadPager/OSWViewController.m
@@ -17,13 +17,19 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+	//  Do any additional setup after loading the view, typically from a nib.
+    //  This is where we will add more.
+
 }
 
 - (void)didReceiveMemoryWarning
 {
+
     [super didReceiveMemoryWarning];
+    
     // Dispose of any resources that can be recreated.
+    
 }
 
 @end
